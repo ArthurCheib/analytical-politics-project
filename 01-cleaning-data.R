86d146c41bcce6677705daa904c3871c01ca4c7d
Arthur Cheib <arthursilvacheib@gmail.com>
2022-02-04 09:36:27 -0600

    Code for cleaning the dataset:
    (i) Putting everyting in a tidy format

---
+++ 01-cleaning-data.R
@@ -0,0 +1,39 @@
+## Loading the necessary packages and installing them, in case you (our TA) don't have it at your computer:
+packages = c("tidyverse", "here", "paletteer", "readxl")
+
+## Now load or install&load all
+lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+rm(packages)
+
+## Getting the data
+data_dictionary <- readxl::read_xlsx(path = "C:/Users/arthu/Downloads/Harris-School-of-PP/Winter quarter (2nd)/ap-psets/country_territory_ratings.xlsx", sheet = 1)
+
+country_ratings <- readxl::read_xlsx(path = "C:/Users/arthu/Downloads/Harris-School-of-PP/Winter quarter (2nd)/ap-psets/country_territory_ratings.xlsx", sheet = 2, col_names = T, skip = 1) %>% 
+  .[,c(1, 68:ncol(.))]
+
+## Cleaning the data
+col_names <- colnames(country_ratings)
+
+possible_status <- str_squish(str_remove(unique(country_ratings$...70), c("Status|-")))[c(2,3,4)]
+possible_values <- str_c(c(1:7, possible_status), collapse = "|")
+
+country_ratings %>%
+  rename(country = col_names[1]) %>% 
+  gather(key, value, -country) %>%
+  mutate(year = as.numeric(ifelse(str_detect(key, "\\..."), NA, key))) %>%
+  select(-key) %>% 
+  mutate(type = ifelse(!str_detect(value, "PR|CL|Status"), NA, value)) %>% 
+  mutate(value = ifelse(str_detect(value, possible_values), value, NA)) %>% 
+  fill(year, type) %>% 
+  spread(type, value) %>% 
+  filter(!is.na(country)) %>% 
+  arrange(country, year)
